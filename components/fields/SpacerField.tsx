6a138bbd1d1e424428932c6011b7cf4d7d2c03ce
6MinhQuan9 <66624412+6MinhQuan9@users.noreply.github.com>
2023-10-30 18:10:03 +0700

    fix items center for spacer element

--- components/fields/SpacerField.tsx
+++ components/fields/SpacerField.tsx
@@ -59,7 +59,7 @@ type propertiesFormSchemaType = z.infer<typeof propertiesSchema>
 function DesignerComponent ({ elementInstance }: { elementInstance: FormElementInstance }) {
    const element = elementInstance as CustomInstance;
    const { height  } = element.extraAttributes;
-   return <div className="flex flex-col gap-2 w-full">
+   return <div className="flex flex-col gap-2 w-full items-center">
       <Label className="text-muted-foreground">Spacer field: {height}px</Label>
       <LuSeparatorHorizontal className='h-8 w-8' />
    </div>
